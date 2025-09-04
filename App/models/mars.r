606942ef193db3a39e7e7fc8afc53424e8940e54
Lucas M Guima <lucasdamata01@hotmail.com>
2025-09-04 10:57:52 -0300

    Correção na geração do modelo

--- App/models/mars.r
+++ App/models/mars.r
@@ -16,12 +16,17 @@ predictors <- args[3:length(args)]
 
 data <- read_csv(file.path("data", file_name))
 
+# Trata os dados
+data[sapply(data, is.infinite)] <- NA # Substitui valores infinitos por NA
+data[sapply(data, is.nan)] <- NA # Substitui valores indefinidos por NA
+data_limpa <- na.omit(data) # Remove as linhas com NA em qualquer coluna
+
 # Transforma os preditores em uma matris
 # Pega os valores numericos das colunas
-x_predictors <- as.matrix(data[predictors])
+x_predictors <- as.matrix(data_limpa[predictors])
 
 # Transforma a coluna de resposta em um vetor
-y_response <- data[[response]]
+y_response <- data_limpa[[response]]
 
 model <- mars(x_predictors, y_response)
 summary(model)
