6a886a6f1476cec577033f3378004644b1d8afa0
Lucas M Guima <lucasdamata01@hotmail.com>
2025-09-08 18:15:11 -0300

    Separa os dados entre teste e treino

--- App/src/call_model.py
+++ App/src/call_model.py
@@ -1,10 +1,34 @@
 import subprocess
+import pandas as pd
+import os
+
+# Separa a data com base na porcentagem entrada para
+# a data de teste, retorna o dataset de teste e
+# treino
+def split_data(train_data_percent: int, data: pd.DataFrame):
+    # Cria o data set de teste
+    test = data.sample(frac=train_data_percent, random_state=42)
+    # Remove o data set de teste do data set de treino
+    train = data.drop(test.index)
+
+    return test, train
 
 def run_model(file_name: str, model: str, response: str, predictors: str|list):
     # Caminho para o script R
     r_script_path = f"models/{model}"
 
-    comando = ["Rscript", r_script_path, file_name, response]
+    # Separa o dataset em treino e teste
+    data = pd.read_csv(f"data/{file_name}")
+    test, train = split_data(20, data)
+
+    # Salva os dataset de treino e teste
+    path_test = os.path.join("data", f"test_{file_name}")
+    path_train = os.path.join("data", f"train_{file_name}")
+    
+    with open(path_test, 'w') as f: f.write(test.to_csv())
+    with open(path_train, 'w') as f: f.write(train.to_csv())
+
+    comando = ["Rscript", r_script_path, path_train, response]
     for p in predictors:
         comando.append(p)
 
