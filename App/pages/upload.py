67694b6d3b01ad061033319ce2403a55d5439078
Lucas M Guima <lucasdamata01@hotmail.com>
2025-09-10 09:34:59 -0300

    Carrega o arquivo no Supabase

--- App/pages/upload.py
+++ App/pages/upload.py
@@ -1,7 +1,7 @@
+import utils.database as database
 import streamlit as st
 import pandas as pd
 import os
-import src.call_model as call
 
 @st.cache_data
 def load_data(file, nrows=100):
@@ -17,12 +17,26 @@ def load_data(file, nrows=100):
 st.title("File Upload")
 
 file = st.file_uploader("Carregar arquivo: ", type=["csv", "xlsx"])
+bucket_name = 'data'
+
+db = database.Conection()
+print(f"ID da conexão: {id(db)}")
 
 if st.button("Carregar"):
-    data = load_data(file)
+    if file is not None:
+        try:
+            response = db.supabase.storage.from_(bucket_name).upload(
+                path=file.name,
+                file=file.read(),
+                file_options={
+                    "cache-control": "3600",
+                    "upsert": "false"  # Set to "true" to replace an existing file
+                }
+            )
+            print(f"Arquivo '{file}' armazenado com sucesso em '{bucket_name}/{file.name}'.")
+            # The response object can contain useful information, but it's often empty on success
+            print("Resposta do Supabase:", response.data)
+        except Exception as e:
+            print(f"Erro ao armazenar arquivo: {e}")
 
-    if data is not None:
-        path = os.path.join("data", file.name)
-        with open(path, 'w') as f:
-            if f.write(data.to_csv()):
-                st.success("Upload Succes")
+        
