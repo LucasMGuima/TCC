a778a3cbb5d33335e7b1c664b4dce1308c2338b9
Lucas <lucasdamata01@hotmail.com>
2025-10-03 16:21:13 -0300

    Ler o Json

--- App/pages/testes.py
+++ App/pages/testes.py
@@ -3,6 +3,7 @@ import src.call_model
 import streamlit as st
 import re
 import pandas as pd
+import json
 
 modelos_disponiveis = filter(lambda x: ".json" in x,  wdg.contentIn_folder('modelos'))
 
@@ -13,14 +14,14 @@ db_avaliableModels = wdg.creat_multselect(
 )
 
 if st.button("Cacular Metricas"):
-    resp = []
     for avaliableModel in db_avaliableModels:
         model = f"modelos/{avaliableModel}"
 
-        resp.append(src.call_model.run_teste(model))
+        src.call_model.run_teste(model)
 
-    qtd_models = len(db_avaliableModels)
-    # Converter o resp (list) para um dicionario
-    for num in range(qtd_models):
-        st.write(f"{db_avaliableModels[num]}:")
-        print(f"{resp[num]}")
+    path = "modelos/resp"
+    jsons = wdg.contentIn_folder(path)
+    for file in jsons:
+        with open(f"{path}/{file}", 'r') as f:
+            data = json.load(f)
+        
